f6884823dd5a8f681ab6bab3cfe19d10dcaf7523
amadeus <amadeus@MacBook-Air-amadeus.local>
2024-11-15 15:12:11 +0300

    23 lesson gate and can

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -47,25 +47,23 @@ class JobController extends Controller
 
     public function edit(Job $job)
     {
-        Gate::define('edit-job', function (User $user, Job $job){
+        Gate::define('edit-job', function (User $user, Job $job) {
 
-            return  $job->employer->user->is($user);
+            return $job->employer->user->is($user);
 
         });
+
+
 
         if (Auth::guest()) {
             return redirect('/login');
         }
-
         Gate::authorize('edit-job', $job);
-
-//        if ($job->employer->user->isNot(Auth::user()))
-//        {
-//            abort(403);
-//        }
-        return view('jobs.edit', ['job' => $job]);
-
+        return view('jobs-edit', ['job => $job']);
     }
+
+
+
 
     public function update(Job $job)
     {
